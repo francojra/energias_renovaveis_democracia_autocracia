ccbdd984f211dde8eecb5b447ff683956f845ea1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-08 21:19:01 -0300

    Add files via upload

---
+++ energias_renovaveis_paises_democratas_autocratas.R
@@ -0,0 +1,35 @@
+
+# Energias renováveis em países democratas e autocratas ------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 08/11/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/renewable-energy ----------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Desde a Revolução Industrial, a combinação de energia da maioria dos países em volta
+### do mundo tem se tornado dominada por combustíveis fósseis. Isso tem grandes implicações 
+### para mudanças climáticas, como bem para a saúde humana. Três-quartos das emissões de gases
+### de efeito estufa resulta da queima de combustíveis fósseis para energia. Combustíveis 
+### fósseis são responsáveis por grande quantidade de poluição do ar local - um problema de
+### saúde que leva a no mínimo 5 milhões de mortes prematuras a cada ano.
+
+### Para reduzir as emissões de CO2 e poluição do ar local, o mundo necessita rapidamente mudar
+### fontes energéticas de baixo-carbono - como tecnologias renováveis e nuclear.
+
+### Energias renováveis tem um papel chave na descarbonização do nosso sistema de energia nas
+### próximas décadas. Mas como rapidamente nossa produção de energias renováveis está mudando?
+### Quais tecnologias parecem mais promissoras em transformar nossa energia?
+
+### Nesse artigo nós observamos os dados sobre tecnologias renováveis no mundo. Qual porcentagem
+### de energia que eles contam hoje e como isso está mudando.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
+library(ggthemes)
+
+
+
+
